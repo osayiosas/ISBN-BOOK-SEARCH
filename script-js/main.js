e20ae1d920bee57478708f32752bed5ffd717756
Osaigbovo idiaghe <idiagheosaigbovoosas@gmail.com>
2022-10-13 08:10:19 +0100

    books isbn search with API

---
+++ script-js/main.js
@@ -0,0 +1,31 @@
+// Building a Book isbn serche API with open library. 
+
+document.querySelector('button').addEventListener('click', getFetch)
+
+function getFetch(){
+    const choice = document.querySelector('input').value
+
+    console.log(choice)
+
+    const url = `https://openlibrary.org/isbn/${choice}.json`
+
+
+    fetch(url)
+
+    .then(res => res.json()) // parse response as json
+
+    .then(data => {
+        if(!localStorage.getItem('books')){
+            localStorage.setItem('books', data.title)
+        }else {
+            let books = localStorage.getItem('books') + data.title
+            localStorage.setItem('books', books)
+        }
+
+        document.querySelector('h2').innerText =localStorage.getItem('books')
+    })
+
+    .catch(err => {
+        console.log(`error ${err}`)
+    });
+}
